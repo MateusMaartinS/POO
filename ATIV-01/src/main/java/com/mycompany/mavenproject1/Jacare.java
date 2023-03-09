110ebd6c30b68f5948a2cdccc7403097d5718dfb
Mateus Martins S <108599241+UnlikeMaartin@users.noreply.github.com>
2023-03-08 21:10:33 -0300

    Update Jacare.java

--- ATIV-01/src/main/java/com/mycompany/mavenproject1/Jacare.java
+++ ATIV-01/src/main/java/com/mycompany/mavenproject1/Jacare.java
@@ -8,10 +8,20 @@ package com.mycompany.mavenproject1;
  *
  * @author aluno
  */
-public class Jacare {
+public class Jacare {  
     String cor;
     int tamanho;
     String especiae;
     char sexo;
     String quanditadeDentes;
+    
+    public void morder(){
+        if ( tamanho >= 8)
+            System.out.print("Esse é bitelo \n CORRE MUITO\n\n");
+        else if (tamanho <3)
+            System.out.print("Esse da medo \n CORRE\n\n");
+        
+        else
+            System.out.print("É um jacaré irmão \n CORRE\n\n");
+        }
 }
